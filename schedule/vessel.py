99ec40180f504b2bd64849a53e8f884cb490b9a5
tim <timlyomaddison@gmail.com>
2015-01-11 15:30:15 +0000

    Started refactoring vessel class

--- schedule/vessel.py
+++ schedule/vessel.py
@@ -8,12 +8,4 @@ class Vessel:
 
     def __init__(self, number):
         self.number = number
-        self.tasks = []
-
-    def addTask(self, task):
-        if isinstance(task, Task):
-            self.tasks.append(task)
-
-    def printVessel(self):
-        for task in self.tasks:
-            task.printTask()
+        self.size
