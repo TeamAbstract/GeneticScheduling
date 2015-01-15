d71c6213dee7d040c3340a678c5281851d60c912
tim <timlyomaddison@gmail.com>
2015-01-15 14:11:48 +0000

    Formatting

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -21,11 +21,9 @@ class Schedule:
 		self.id = Schedule.id
 		Schedule.id += 1
 
-
 	def getTaskCount(self):
 		"""Returns the number of tasks that the schedule has defined"""
 		return len(self.tasks)
-
 
 	def getTask(self, index=None):
 		"""Returns a specific task or if id==None then all tasks as a tuple
@@ -41,13 +39,11 @@ class Schedule:
 		else:
 			return self.tasks[index]
 
-
 	def printSchedule(self):
 		print("Schedule:", self.id)
 		for task in self.tasks:
 			print("Task: ", task.product.name)
 			task.printTask()
-
 
 	def addTask(self, task):
 		assert isinstance(task, Task)
