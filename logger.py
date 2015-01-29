c4c0084da44835ac299a5a1fa0a1f0a55c8e6e66
tim <timlyomaddison@gmail.com>
2015-01-29 20:03:46 +0000

    Moved logger functions into their own class

---
+++ logger.py
@@ -0,0 +1,10 @@
+def saveHigh():
+        highSave=open('c:/HighPriorityIsddfsdsues.txt', 'w')
+
+
+def saveMedium():
+    mediumSave=open('c:/MediumPriorityIssues.txt', 'w')
+
+
+def saveLow():
+    lowSave=open('c:/LowPriorityIssues.txt', 'w')
