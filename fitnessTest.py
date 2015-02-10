6116e439bc1e16e205938655d8a696270e061e7a
juncraul <juncraul@yahoo.com>
2015-02-10 19:13:06 +0000

    Update fitnessTest.py
    Added the 4 fitness functions

--- fitnessTest.py
+++ fitnessTest.py
@@ -40,6 +40,47 @@ def testSchedule(schedule):
     schedule.fitness = fitness
 
     return fitness
+    
+    def TimeToDeadline(schedule):
+        score = 0
+        for x in (0, schedule.getTaskCount()):
+            task = schedule.getTask(x)
+            score += (task.getEndTime() + task.Cooldown - task.getDeadline()).getTotalHours() * schedule.bonusPerHourTillDeadline
+        return score
+
+    def IsOverDeadline(schedule):
+        score = 0
+        for x in (0, schedule.getTaskCount()):
+            task = schedule.getTask(x)
+            partialScore = (task.getEndTime() + task.Cooldown - task.getDeadline()).getTotalHours() * schedule.penaltyPerHourOverDeadline
+            if partialScore > 0:
+                score += partialScore
+        return score
+
+    def IsOverlapping(schedule):
+        score = 0
+        for x in (0, schedule.getTaskCount() - 1):
+            for y in (x + 1, schedule.getTaskCount()):
+                taskX = schedule.getTask(x)
+                taskY = schedule.getTask(y)
+                hoursOverlapping = taskX.getStartTime - (taskX.getEndTime + taskX.Cooldown)
+                if taskX.getVessele == taskY.getVessele and hoursOverlapping > 0:
+                    score += hoursOverlapping * schedule.penaltyPerHourOverlapping
+        return score
+
+    def CheckIdleVessels(schedule):
+        score = 0
+        for vessel in Vessels.vessels:
+            timeCurrent = None
+            for i in (0, schedule.getTaskCount() - 1):
+                task = schedule.getTask(i)
+                if task.getVessel() == vessel:
+                    if timeCurrent == None:
+                        timeCurrent = task.getEndTime() + task.Cooldown
+                    else:
+                        score += (task.getStartTime() - timeCurrent).getTotalHours() * schedule.penaltyPerHourIdle
+        return score
+
 
 
 
