8a3cb9e234cf1eb76c638c4dbb4cc4dca709dffe
stevenellul <stevenellul@googlemail.com>
2015-02-18 22:24:36 +0000

    some form of logger working :)

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ vessels.addVessel(20)
 vessels.addVessel(30)
 vessels.addVessel(50)
 
-saveHigh('\nlots of issues')
+saveHigh('\nlots of issu9es')
 saveHigh('\nlots of other iss7ues')
 saveHigh('\nlots of other iss6ues')
 saveHigh('\nlots of other iss5ues')
