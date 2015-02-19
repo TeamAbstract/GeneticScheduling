e3a7521f85f4e40ab0333fde0e075576de6e3737
tim <timlyomaddison@gmail.com>
2015-02-19 15:07:59 +0000

    removed temp build folder

--- main.build/__helpers.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __NUITKA_CALLS_H__
-#define __NUITKA_CALLS_H__
-
-extern PyObject *CALL_FUNCTION_WITH_ARGS1( PyObject *called, PyObject *arg0 );
-extern PyObject *CALL_FUNCTION_WITH_ARGS2( PyObject *called, PyObject *arg0, PyObject *arg1 );
-extern PyObject *CALL_FUNCTION_WITH_ARGS3( PyObject *called, PyObject *arg0, PyObject *arg1, PyObject *arg2 );
-extern PyObject *CALL_FUNCTION_WITH_ARGS5( PyObject *called, PyObject *arg0, PyObject *arg1, PyObject *arg2, PyObject *arg3, PyObject *arg4 );
-#endif
