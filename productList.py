8c8eb79bf98674f4d4cfcaa6315931001cd41280
DarrenBlack <whiteon01@gmail.com>
2015-04-19 16:31:24 +0100

    Update productList.py

--- productList.py
+++ productList.py
@@ -2,10 +2,18 @@ from product import Product
 
 
 masterProductList = [
-	Product("Booze", "1", 10),
-	Product("Hooch", "2", 30),
-	Product("Brew", "4", 15),
-	Product("Beer", "4", 60)]
+	Product("Porter", "1", 10),
+	Product("Mild", "2", 30),
+	Product("Kölsch", "4", 15),
+	Product("Pilsener", "4", 60),
+	Product("Lager", "3", 40),
+	Product("Bitter", "4", 45),
+	Product("Stout", "5", 20),
+	Product("Brown Ale", "2", 25),
+	Product("Blonde Ale", "1", 35),
+	Product("Golden Ale", "2", 15),
+	Product("Red Ale", "2", 37),
+	Product("Light Ale", "3", 50)]
 
 
 def getByName(search):
