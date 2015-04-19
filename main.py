1f18d27edb64b9bfc4f8e1bc5b96aaf0f201ee22
tim <timlyomaddison@gmail.com>
2015-04-19 16:22:35 +0100

    refactored main

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-from datetime import *
-
-import genetics
 from vessels import Vessels
 from gui.mainWindow import startGUI
 
 
-vessels = Vessels()
-vessels.addVessel(20)
-vessels.addVessel(30)
-vessels.addVessel(50)
+def init():
+	vessels = Vessels()
+	vessels.addVessel(20)
+	vessels.addVessel(30)
+	vessels.addVessel(50)
 
 
+
+init()
 startGUI()
-
-startTime = datetime.now()
-
-
-# genepool.addSchedule(genetics.getNewSchedule())
-#
-# for _count in range(200):  # temp loop to run algorithm
-# 	genepool.refreshSchedules()
-# 	genetics.FitnessTest.testPool(genepool)
-# 	print("Best result", genepool.getBestSchedule().fitness)
-# 	genepool.removeSchedules()
-
-# genepool.printPool()
-
-# print("Best Schedule")
-# genepool.getBestSchedule().print()
-#
-# print("Ran in ", round((datetime.now() - startTime).total_seconds(), 2), " seconds")
